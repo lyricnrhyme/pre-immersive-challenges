2342fc4e83e2c7d891fb4c4939f840330b668db2
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-04 21:44:21 -1000

    Completed Exercise

--- check-ascending-order/checkAscOrder.js
+++ check-ascending-order/checkAscOrder.js
@@ -1,3 +1,12 @@
 module.exports = function checkAscOrder(numArray) {
   // write your code in here
+for (var i=1; i < numArray.length; i++) {
+  if (numArray[i] > numArray[i-1]) {
+    console.log("true");
+    return true;
+  } else {
+    console.log("false");
+    return false;
+  }
+}
 }
