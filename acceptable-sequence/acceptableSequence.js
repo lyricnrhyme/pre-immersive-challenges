9da0c6e72616ae2417a73d93feee9050751ac2c4
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-04 21:33:58 -1000

    Solved the problem

--- acceptable-sequence/acceptableSequence.js
+++ acceptable-sequence/acceptableSequence.js
@@ -1,3 +1,30 @@
+
 module.exports = function acceptableSequence(str) {
   // write your code here
-}
+  var splitUp = str.split("");
+  // console.log(splitUp);
+  var newStr = [];
+ for (var i=0; i < splitUp.length; i++) {
+  if (splitUp[i] == "+") {
+    newStr.push("Yay");
+    //  console.log("Yay");
+   } else if (splitUp[i] == "=") {
+    newStr.push("Nay");
+    //  console.log("Nay");
+   } else {
+    newStr.push("Letter");
+    //  console.log("Letter");
+   };
+
+   };
+  //  console.log(newStr);
+   for (var i=0; i < newStr.length; i++) {
+     if (newStr[i] == "Letter") {
+       if (newStr[i-1] == "Yay" && newStr[i+1] == "Yay") {
+         return true;
+       } else {
+         return false;
+       };
+     };
+    };
+  };
