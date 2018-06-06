2de79390058a471097a2d8c7130bd8a72684964a
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 00:24:10 -1000

    Solved challenge but looking for other methods

--- descending-order/descendingOrder.js
+++ descending-order/descendingOrder.js
@@ -1,4 +1,23 @@
 module.exports = function descendingOrder(number) {
   // write code in here
-console.log(typeof number);
+  // console.log(typeof number);
+
+  if (typeof number === "string") {
+    // console.log("not a number!");
+    var notNumber = "not a number!";
+    return notNumber;
+  }
+  var digits = number.toString().split("");
+  // console.log(digits);
+  var digitsSort = digits.sort();
+  // console.log(digitsSort);
+
+  var descending = []
+
+  for (var i=digits.length-1; i >= 0; i--) {
+    descending.push(digitsSort[i]);
+  }
+  // console.log(descending);
+  var newNum = descending.join("");
+  return newNum;
 }
