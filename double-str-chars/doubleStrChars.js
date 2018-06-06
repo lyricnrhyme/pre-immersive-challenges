b0c6d4ee8ae8c1b20e943e2dccdc825c78f91f1c
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 09:53:24 -1000

    Split strings and added for loop for doubling

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -2,7 +2,12 @@ module.exports = function doubleStrChars(str) {
   // write your code in here
   console.log(str);
   if (typeof str === "string") {
-    console.log("i'm a string!")
+    // console.log("i'm a string!");
+    var splitStr = str.split("");
+    console.log(splitStr);
+    for (var i=0; i < splitStr.length; i++) {
+      
+    }
   } else {
     var notStr = "not a string!";
     return notStr;
