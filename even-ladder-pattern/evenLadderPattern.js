60766bc484db45b09352af6fa4d303d59f3531a9
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 10:13:20 -1000

    Returned '' for 1,0 and negative number. Created for loop for finding even numbers

--- even-ladder-pattern/evenLadderPattern.js
+++ even-ladder-pattern/evenLadderPattern.js
@@ -1,3 +1,12 @@
 module.exports = function evenLadderPattern(num) {
   // write your code in here
+  console.log(num);
+  if (num <=1) {
+    return "";
+  } else {
+    // console.log(typeof num);
+    for (var i=0; i<num; i++){
+      console.log("num");
+    }
+  }
 }
