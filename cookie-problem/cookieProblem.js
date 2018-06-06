36a0e2ddb678a0b6b5ef94f3fdc4c318532492d3
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-05 18:56:09 -1000

    Found difference

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,11 +1,17 @@
 module.exports = function cookieProblem(array) {
   // write your code in here
-  console.log(array);
+  // console.log(array);
   var bigNum = array[0]
   for (var i=1; i < array.length; i++) {
     if (array[i] > bigNum) {
       bigNum = array[i];
     };
+  };
+  // console.log(bigNum);
+
+  var beFair = 0;
+  for (var i=0; i < array.length; i++) {
+    var difference = bigNum - array[i];
+    console.log(difference);
   }
-  console.log(bigNum);
 }
