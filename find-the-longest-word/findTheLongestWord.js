f516357dec5059ad445068b866c2a3efb6ab9d4a
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 11:30:08 -1000

    Found longest word

--- find-the-longest-word/findTheLongestWord.js
+++ find-the-longest-word/findTheLongestWord.js
@@ -1,3 +1,13 @@
 module.exports = function findTheLongestWord(sen) {
   // write your code in here
+  console.log(sen);
+  var longestWord = ""
+  for (var i=0; i<sen.length; i++) {
+    // console.log(sen[i]);
+    if (sen[i].length > longestWord.length) {
+      longestWord = sen[i];
+    }
+  }
+  // console.log(longestWord);
+  return longestWord;
 }
