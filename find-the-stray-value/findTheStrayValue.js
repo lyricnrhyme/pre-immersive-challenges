e7f2e9e94d7d34ca1b093e888e033b43ce3c3e79
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 13:17:28 -1000

    Found stray value, will look into other methods

--- find-the-stray-value/findTheStrayValue.js
+++ find-the-stray-value/findTheStrayValue.js
@@ -1,3 +1,13 @@
 module.exports = function findTheStrayValue(array) {
   // write your code in here
+  console.log(array);
+  var diffWord = ""
+  for (var i=0; i<array.length; i++) {
+    // console.log(array[i]);
+    if (array[i] !== array[i+1] && array[i] !== array[i-1]) {
+      diffWord = array[i];
+    }
+  }
+  console.log(diffWord);
+  return diffWord;
 }
