a3168659bf185806b1bf1f42a3789bb186d6536e
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-05 23:30:23 -1000

    Completed Multiples

--- count-by-multiples/countByMultiples.js
+++ count-by-multiples/countByMultiples.js
@@ -1,3 +1,11 @@
 module.exports = function countByMultiples(length, x) {
   // write your code in here
+  // console.log(length);
+  // console.log(x);
+  var multArray = [];
+  for (var i=1; i <= length; i++) {
+    // console.log(x*i);
+    multArray.push(x*i);
+  }
+  return multArray
 }
