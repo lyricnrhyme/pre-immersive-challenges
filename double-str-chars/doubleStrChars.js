401ffa744906ff66e3c4a010442ab56d7e6c89fa
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 09:50:11 -1000

    Created base format

--- double-str-chars/doubleStrChars.js
+++ double-str-chars/doubleStrChars.js
@@ -1,3 +1,10 @@
 module.exports = function doubleStrChars(str) {
   // write your code in here
+  console.log(str);
+  if (typeof str === "string") {
+    console.log("i'm a string!")
+  } else {
+    var notStr = "not a string!";
+    return notStr;
+  };
 }
