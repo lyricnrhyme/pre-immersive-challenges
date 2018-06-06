e0e6ecdf2beab5011bcc701effe9c80e6d762dd7
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-05 14:04:39 -1000

    Found the biggest number in array

--- cookie-problem/cookieProblem.js
+++ cookie-problem/cookieProblem.js
@@ -1,3 +1,11 @@
 module.exports = function cookieProblem(array) {
   // write your code in here
+  console.log(array);
+  var bigNum = array[0]
+  for (var i=1; i < array.length; i++) {
+    if (array[i] > bigNum) {
+      bigNum = array[i];
+    };
+  }
+  console.log(bigNum);
 }
