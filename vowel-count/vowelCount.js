4dea32d0a5426e0fb634ceb402d7258e6df08cb8
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 23:24:16 -1000

    Learned about .match(/[aeiouAEIOU]/), found amount of vowels in string

--- vowel-count/vowelCount.js
+++ vowel-count/vowelCount.js
@@ -1,3 +1,16 @@
 module.exports = function vowelCount(str) {
   // write your code in here
+  console.log(str);
+  var splitStr = str.split("");
+  // console.log(splitStr);
+  var totalVowel = 0;
+  for (var i=0; i<splitStr.length; i++) {
+    if (splitStr[i].match(/[aeiouAEIOU]/)) {
+      totalVowel += 1;
+    } else {
+      totalVowel += 0;
+    }
+  }
+  console.log(totalVowel);
+  return totalVowel;
 }
