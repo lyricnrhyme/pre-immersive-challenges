14f2232de871d511cfd394bfebdd34950149f792
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-06 20:58:17 -1000

    Reversed string, attempting to capitalize specific words in string

--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -2,5 +2,23 @@ module.exports = function reverseMessage(str) {
   // write your code in here
   // console.log(str);
   var lowerCase = str.toLowerCase();
-  // console.log(lowerCase);
+  console.log(lowerCase);
+  var strArr = lowerCase.split(" ");
+  console.log(strArr);
+  var newStr = [];
+  for (var i=strArr.length-1; i>=0; i--) {
+    var character = strArr[i].split("");
+    // console.log(character);
+    var newWord = [];
+    for (var j=strArr[i].length-1; j>=0; j--) {
+      newWord.push(strArr[i][j]);
+      // console.log(newWord);
+    }
+    var reversedWord = newWord.join("");
+    console.log(reversedWord);
+    newStr.push(reversedWord);
+    
+  }
+  // var reversedStr = newStr.join(" ");
+  // console.log(reversedStr);
 }
