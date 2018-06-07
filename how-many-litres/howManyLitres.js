51ccbdaf8b09036e46cafc972ac63facc2a90758
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 14:28:18 -1000

    Learned about rounding down, found amount of litres per hour

--- how-many-litres/howManyLitres.js
+++ how-many-litres/howManyLitres.js
@@ -1,3 +1,12 @@
 module.exports = function howManyLitres(hours) {
   // write your code in here
+  // console.log(hours);
+  var litres = 0;
+  for (var i=0; i<hours; i++) {
+    litres += 0.5;
+  }
+  // console.log(litres);
+  var estLitres = Math.floor(litres);
+  console.log(estLitres);
+  return estLitres;
 }
