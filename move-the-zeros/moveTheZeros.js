f4d8ea9c948808db9121b4336412ac79eb78aeb3
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 15:22:09 -1000

    Separated zeros and non-zeros, will combine based on isRight

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,17 +1,28 @@
 module.exports = function moveTheZeros(arrNum, isRight) {
   // write your code here
   console.log(arrNum, isRight);
+  var notZeros = [];
   var zeros = [];
   for (var i=0; i<arrNum.length; i++) {
-    if (arrNum[i] == 0) {
+    if (arrNum[i] != 0) {
+      notZeros.push(arrNum[i]);
+    } else if (arrNum[i] == 0) {
       zeros.push(0);
     }
-    if (isRight == true) {
-      // console.log("move right");
-    } else {
-      // console.log("move left");
-    }
   }
-  console.log(zeros);
-  console.log(arrNum);
+    
+  if (isRight == true) {
+    // console.log("move right");
+    for (var i=0; i<zeros.length; i++) {
+      notZeros.push(zeros[i]);
+    }
+  } else {
+    for (var i=0; i<notZeros.length; i++) {
+      zeros.push(notZeros[i]);
+    }
+    // console.log("move left");
+  }
+
+  // console.log(notZeros);
+  // console.log(zeros);
 }
