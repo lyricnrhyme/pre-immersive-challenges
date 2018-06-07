b93b0663367cea423f94c97c1b0eb40ff1e7f367
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 14:57:39 -1000

    Wrote out isRight formula

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,3 +1,9 @@
 module.exports = function moveTheZeros(arrNum, isRight) {
   // write your code here
+  console.log(arrNum, isRight);
+  if (isRight == true) {
+    console.log("move right");
+  } else {
+    console.log("move left");
+  }
 }
