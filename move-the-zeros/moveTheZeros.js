8aba6c26015693d5d5764ceadb2b0a53e12036de
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 15:09:17 -1000

    Pushed new zeros into new array, figuring out how to remove zeros from arrNum

--- move-the-zeros/moveTheZeros.js
+++ move-the-zeros/moveTheZeros.js
@@ -1,9 +1,17 @@
 module.exports = function moveTheZeros(arrNum, isRight) {
   // write your code here
   console.log(arrNum, isRight);
-  if (isRight == true) {
-    console.log("move right");
-  } else {
-    console.log("move left");
+  var zeros = [];
+  for (var i=0; i<arrNum.length; i++) {
+    if (arrNum[i] == 0) {
+      zeros.push(0);
+    }
+    if (isRight == true) {
+      // console.log("move right");
+    } else {
+      // console.log("move left");
+    }
   }
+  console.log(zeros);
+  console.log(arrNum);
 }
