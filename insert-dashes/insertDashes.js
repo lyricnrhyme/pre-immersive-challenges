d3c21747a8bf878b7691b7a4323d13ff8354a866
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 14:49:10 -1000

    Inserted dashes into array version, will merge into one string

--- insert-dashes/insertDashes.js
+++ insert-dashes/insertDashes.js
@@ -1,3 +1,16 @@
 module.exports = function insertDashes(num) {
   // write your code in here
+  // console.log(num);
+  var numStr = num.toString();
+  // console.log(numStr);
+  var splitNum = numStr.split("");
+  // console.log(splitNum);
+
+  for (var i=0; i<splitNum.length; i++) {
+    if ((splitNum[i]%2) == 0 && (splitNum[i+1]%2) == 0) {
+      splitNum.splice(i+1, 0, "-");  
+      // console.log("dash");
+    }
+  }
+  console.log(splitNum);
 }
