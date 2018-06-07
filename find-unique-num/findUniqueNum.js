a39ad32ee27217355e080dd169f3fc7c8c3c831d
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 14:09:18 -1000

    Changed string into array, found unique number

--- find-unique-num/findUniqueNum.js
+++ find-unique-num/findUniqueNum.js
@@ -1,3 +1,24 @@
 module.exports = function findUniqueNum(strOfNums) {
   // write your code in here
+  console.log(strOfNums);
+  console.log(typeof strOfNums);
+  var numArr = strOfNums.split(" ");
+  console.log(numArr);
+  var uniqueNum = 0
+  // var numCheck = [];
+  // for (var i=0; i<numArr.length; i++) {
+  //   if ((numArr[i]%2) == 0) {
+  //     numCheck.push("even");
+  //   } else {
+  //     numCheck.push("odd");
+  //   }
+  // }
+  // console.log(numCheck);
+  for (var i=0; i<numArr.length; i++) {
+    if ((numArr[i]%2) != (numArr[i+1]%2) && (numArr[i]%2) != (numArr[i-1]%2)) {
+      uniqueNum = i;
+    }
+  }
+  console.log(uniqueNum);
+  return uniqueNum;
 }
