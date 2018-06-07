ab3b6c44bcbdc0107cd22db3fa198bfadc61b03f
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-06 23:03:22 -1000

    Capitalized words, merged string back together in reverse order

--- reverse-message/reverseMessage.js
+++ reverse-message/reverseMessage.js
@@ -2,9 +2,9 @@ module.exports = function reverseMessage(str) {
   // write your code in here
   // console.log(str);
   var lowerCase = str.toLowerCase();
-  console.log(lowerCase);
+  // console.log(lowerCase);
   var strArr = lowerCase.split(" ");
-  console.log(strArr);
+  // console.log(strArr);
   var newStr = [];
   for (var i=strArr.length-1; i>=0; i--) {
     var character = strArr[i].split("");
@@ -12,13 +12,17 @@ module.exports = function reverseMessage(str) {
     var newWord = [];
     for (var j=strArr[i].length-1; j>=0; j--) {
       newWord.push(strArr[i][j]);
-      // console.log(newWord);
+      // console.log(newWord); 
     }
+    // console.log(newWord);
+    newWord[0] = newWord[0].toUpperCase();
+    console.log(newWord);
     var reversedWord = newWord.join("");
-    console.log(reversedWord);
+    // console.log(reversedWord);
     newStr.push(reversedWord);
-    
   }
-  // var reversedStr = newStr.join(" ");
-  // console.log(reversedStr);
+  console.log(newStr);
+  var reversedStr = newStr.join(" ");
+  console.log(reversedStr);
+  return reversedStr;
 }
