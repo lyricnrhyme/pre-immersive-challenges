3f2c4329cec18f1efc7177b41267b20e0852ced6
Jamie Yamashita <jamieyamashita@Jamies-MacBook-Pro.local>
2018-06-06 20:36:36 -1000

    Learned about includes(), used to find needle within array, confirmed locations of existing needles

--- needle-in-haystack/needleInHaystack.js
+++ needle-in-haystack/needleInHaystack.js
@@ -1,3 +1,15 @@
 module.exports = function needleInHaystack(haystack) {
   // write code in here
+  console.log(haystack);
+  if (haystack.includes("needle")) {
+    for (var i=0; i<haystack.length; i++) {
+        if (haystack[i] == "needle") {
+          var foundNeedle = i;
+          return foundNeedle;
+        }
+    }
+  } else {
+    return false;
+    } 
+  console.log(haystack.includes("needle"))
 }
