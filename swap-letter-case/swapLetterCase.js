b60f83251d8d3126538e046b0b6ec783a3c2f570
Jamie Yamashita <jamieyamashita@Jamies-MBP.hawaii.rr.com>
2018-06-06 23:10:02 -1000

    Swapped all letters from lowercase to uppercase and vice versa

--- swap-letter-case/swapLetterCase.js
+++ swap-letter-case/swapLetterCase.js
@@ -1,3 +1,16 @@
 module.exports = function swapLetterCase(str) {
   // write your code in here
+  console.log(str);
+  var splitStr = str.split("");
+  console.log(splitStr);
+  for (var i=0; i<splitStr.length; i++) {
+    if (splitStr[i] == splitStr[i].toLowerCase()) {
+      splitStr[i] = splitStr[i].toUpperCase();
+    } else {
+      splitStr[i] = splitStr[i].toLowerCase();
+    }
+  }
+  console.log(splitStr);
+  var swap = splitStr.join("");
+  return swap;
 }
